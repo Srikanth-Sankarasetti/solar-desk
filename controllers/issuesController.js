4d9f2619128968990da7d885f73bfe7474abfb97
sankarasetti sriaknth <learnnew0449@gmail.com>
2025-07-05 11:59:02 +0530

    some code modified

--- controllers/issuesController.js
+++ controllers/issuesController.js
@@ -401,9 +401,7 @@ exports.downloadExcelReport = catchAsync(async (req, res, next) => {
   if (plantType) filter["plant.plantType"] = plantType;
   if (zone) filter["plant.Zone"] = zone;
   if (plantOwner) filter["plant.plantOwner"] = plantOwner;
-  if (user.role !== "admin" && user.role !== "manager") {
-    filter["assignedEngineer"] = user._id;
-  }
+
   const pipeline = [];
   if (Object.keys(filter).length > 0) {
     pipeline.push({
